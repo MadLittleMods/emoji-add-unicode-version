efcb7c867dedb26ce74c3dfa5904bbd6b784d219
Eric Eastwood <contact@ericeastwood.com>
2017-02-01 22:17:53 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Add `unicode_version` to emoji JSON map
+
+```
+npm start
+```
+
+`emojis.json` is from https://github.com/jonathanwiesel/gemojione/blob/3d5dc04e9eae5a85e1e2de6727da6a882b79f256/config/index.json
+
+`dist/emojis.json` holds the transformed data with `unicode_version` property
+
+
+## Some differences
+
+I noticed a few differences from the [gemojione](https://github.com/jonathanwiesel/gemojione/blob/3d5dc04e9eae5a85e1e2de6727da6a882b79f256/config/index.json) and [emojione](https://github.com/Ranks/emojione/blob/1a3227ac635b0fc456985b3a00e63492c225d254/lib/js/emojione.js) JSON maps and updated to the latest emojione.
+
+ - `large_blue_circle` -> `blue_circle`: https://github.com/Ranks/emojione/commit/dbf2df55150fbcce295e480aede2c603fb51ec12
+ - `ten` -> `keycap_ten`: https://github.com/Ranks/emojione/commit/48ab43b4af82f752a84065913ee6b47b01398b86
---
+++ index.js
@@ -0,0 +1,129 @@
+const Promise = require('bluebird');
+const fs = require('fs-extra');
+const readFile = Promise.promisify(fs.readFile);
+const outputFile = Promise.promisify(fs.outputFile);
+require('isomorphic-fetch');
+const sanitizeForFileName = require('sanitize-filename');
+const cheerio = require('cheerio');
+const emojione = require('emojione');
+
+const emojiMap = require('./emojis.json');
+
+function unicodeToName(emojiUnicode) {
+	const emojiShortName = emojione.toShort(emojiUnicode);
+	const emojiName = emojiShortName.slice(1, emojiShortName.length - 1);
+	return emojiName;
+}
+
+
+const emojipediaUnicodeVersionUrlMap = {
+	1.1: 'http://emojipedia.org/unicode-1.1/',
+	3.0: 'http://emojipedia.org/unicode-3.0/',
+	3.2: 'http://emojipedia.org/unicode-3.2/',
+	4.0: 'http://emojipedia.org/unicode-4.0/',
+	4.1: 'http://emojipedia.org/unicode-4.1/',
+	5.1: 'http://emojipedia.org/unicode-5.1/',
+	5.2: 'http://emojipedia.org/unicode-5.2/',
+	6.0: 'http://emojipedia.org/unicode-6.0/',
+	6.1: 'http://emojipedia.org/unicode-6.1/',
+	7.0: 'http://emojipedia.org/unicode-7.0/',
+	8.0: 'http://emojipedia.org/unicode-8.0/',
+	9.0: 'http://emojipedia.org/unicode-9.0/',
+	10.0: 'http://emojipedia.org/unicode-10.0/',
+	11.0: 'http://emojipedia.org/unicode-11.0/'
+};
+
+
+const addingEmojipediaUnicodeVersionPromises = Object.keys(emojipediaUnicodeVersionUrlMap).map((unicodeVersionKey) => {
+	console.log(`Working on ${unicodeVersionKey}...`)
+	const emojipediaUrl = emojipediaUnicodeVersionUrlMap[unicodeVersionKey];
+	const cacheFileName = sanitizeForFileName(emojipediaUrl);
+	const cacheFilePath = `./cached-pages/${cacheFileName}.html`;
+	// Try to read from the cache
+	return readFile(cacheFilePath, 'utf8')
+		// Fallback to fetching the page again
+		.catch(() => {
+			return fetch(emojipediaUrl)
+				.then((res) => res.text())
+				.then((html) => {
+					// Save the file as a cache
+					outputFile(cacheFilePath, html);
+					return html;
+				});
+		})
+		.then((html) => {
+			scrapeEmojipediaHtml(emojiMap, html, parseFloat(unicodeVersionKey));
+		})
+});
+
+// Save the transformed emoji map
+Promise.all(addingEmojipediaUnicodeVersionPromises)
+	.then(() => {
+		console.log('Filling out ZWJ sequences...');
+		fillZwjSequenceUnicodeVersions(emojiMap);
+	})
+	.then(() => {
+		// Verify each emoji has a `unicode_version`
+		let missingVersionCount = 0;
+		Object.keys(emojiMap).forEach((emojiNameKey) => {
+			const emoji = emojiMap[emojiNameKey];
+			if(emoji.unicode_version === undefined) {
+				console.log(`Missing unicode_version property on \`${emojiNameKey}\``);
+				missingVersionCount += 1;
+			}
+		});
+		if(missingVersionCount > 0) {
+			console.log(`Missing unicode_version property on ${missingVersionCount} emojis`);
+		}
+	})
+	.then(() => {
+		return outputFile('./dist/emojis.json', JSON.stringify(emojiMap, null, 2));
+	});
+
+
+
+function scrapeEmojipediaHtml(emojiMap, html, unicodeVersion) {
+	const $ = cheerio.load(html);
+	$('.container .content li')
+		.each((index, el) => {
+			const $el = $(el);
+			const emojiUnicodeEl = $el.find('.emoji');
+			if(emojiUnicodeEl.length) {
+				// Get rid of the emoji presentation selectors U+FE0F at the end
+				const emojiUnicode = emojiUnicodeEl.text().replace(/(\uFE0F)+$/g, '');
+				const emojiNameKey = unicodeToName(emojiUnicode);
+				if(emojiMap[emojiNameKey]) {
+					emojiMap[emojiNameKey].unicode_version = unicodeVersion;
+				}
+			}
+		});
+}
+
+
+function fillZwjSequenceUnicodeVersions(emojiMap) {
+	Object.keys(emojiMap).forEach((emojiNameKey) => {
+		const emoji = emojiMap[emojiNameKey];
+		const emojiHexCodePoints = emoji.unicode.split('-');
+
+		// Flags are ZWJ sequences made up of regional indicator symbol letters
+		// See http://emojipedia.org/unicode-6.0/
+		if (emojiNameKey.indexOf('flag_') === 0) {
+			emoji.unicode_version = 6.0;
+		}
+		// If general ZWJ sequence
+		else if(!emoji.unicode_version && emojiHexCodePoints.length > 1) {
+			// Find the emoji of the ZWJ sequence with the highest unicode version
+			emoji.unicode_version = emojiHexCodePoints.reduce((unicodeVersion, hexCodePoint) => {
+				var emojiUnicode = String.fromCodePoint(parseInt(hexCodePoint, 16));
+				const emojiNameKey = unicodeToName(emojiUnicode);
+				const emoji = emojiMap[emojiNameKey];
+
+				if(emoji && emoji.unicode_version > unicodeVersion) {
+					unicodeVersion = emoji.unicode_version;
+				}
+
+				return unicodeVersion;
+			}, 0) || false;
+		}
+	});
+}
